96a95cee01afdbb09127f23fdce9832d076d85d6
Ira <nikulochkina.irina@yandex.ru>
2023-07-10 16:21:31 +0300

    finished first video

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './img/logo.svg';
 import profile from './img/profile.svg';
 import cart from './img/cart.svg';
 import sneakers1 from './img/sneakers1.jpg';
+import sneakers2 from './img/sneakers2.jpg';
+import sneakers3 from './img/sneakers3.jpg';
 import addButton from './img/addButton.svg';
 
 function App() {
@@ -28,48 +30,51 @@ function App() {
                 </header>
                 <div className={'content'}>
                     <h1>Все кроссовки</h1>
-                    <div className={'card'}>
-                        <img width={133} height={112} alt={''} src={sneakers1}/>
-                        <h5>Мужские кроссовки Nike Blazer
-                        </h5>
-                        <div className={'cardBottom'}>
-                            <div className={'cardBottomText'}>
-                                <span>Цена:</span>
-                                <b>5 руб.</b>
+                    <div className={'sneakers'}>
+                        <div className={'card'}>
+                            <img width={133} height={112} alt={''} src={sneakers1}/>
+                            <h5>Мужские кроссовки Nike Blazer
+                            </h5>
+                            <div className={'cardBottom'}>
+                                <div className={'cardBottomText'}>
+                                    <span>Цена:</span>
+                                    <b>5 руб.</b>
+                                </div>
+                                <button className={'addButton'}>
+                                    <img width={25} height={25} alt={''} src={addButton}/>
+                                </button>
                             </div>
-                            <button className={'addButton'}>
-                                <img width={25} height={25} alt={''} src={addButton}/>
-                            </button>
                         </div>
+                        <div className={'card'}>
+                            <img width={133} height={112} alt={''} src={sneakers2}/>
+                            <h5>Мужские кроссовки Nike Blazer
+                            </h5>
+                            <div className={'cardBottom'}>
+                                <div className={'cardBottomText'}>
+                                    <span>Цена:</span>
+                                    <b>5 руб.</b>
+                                </div>
+                                <button className={'addButton'}>
+                                    <img width={25} height={25} alt={''} src={addButton}/>
+                                </button>
+                            </div>
+                        </div>
+                        <div className={'card'}>
+                            <img width={133} height={112} alt={''} src={sneakers3}/>
+                            <h5>Мужские кроссовки Nike Blazer
+                            </h5>
+                            <div className={'cardBottom'}>
+                                <div className={'cardBottomText'}>
+                                    <span>Цена:</span>
+                                    <b>5 руб.</b>
+                                </div>
+                                <button className={'addButton'}>
+                                    <img width={25} height={25} alt={''} src={addButton}/>
+                                </button>
+                            </div>
+                        </div>
+                        <div/>
                     </div>
-                    {/*<div className={'card'}>*/}
-                    {/*    <img width={133} height={112} alt={''} src={sneakers2}/>*/}
-                    {/*    <h5>Мужские кроссовки Nike Blazer*/}
-                    {/*    </h5>*/}
-                    {/*    <div className={'cardBottom'}>*/}
-                    {/*        <div className={'cardBottomText'}>*/}
-                    {/*            <span>Цена:</span>*/}
-                    {/*            <b>5 руб.</b>*/}
-                    {/*        </div>*/}
-                    {/*        <button className={'addButton'}>*/}
-                    {/*            <img width={25} height={25} alt={''} src={addButton}/>*/}
-                    {/*        </button>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
-                    {/*<div className={'card'}>*/}
-                    {/*    <img width={133} height={112} alt={''} src={sneakers3}/>*/}
-                    {/*    <h5>Мужские кроссовки Nike Blazer*/}
-                    {/*    </h5>*/}
-                    {/*    <div className={'cardBottom'}>*/}
-                    {/*        <div className={'cardBottomText'}>*/}
-                    {/*            <span>Цена:</span>*/}
-                    {/*            <b>5 руб.</b>*/}
-                    {/*        </div>*/}
-                    {/*        <button className={'addButton'}>*/}
-                    {/*            <img width={25} height={25} alt={''} src={addButton}/>*/}
-                    {/*        </button>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
                 </div>
             </div>
         </div>
